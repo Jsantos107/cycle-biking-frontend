7bcf4a0e843a8140b0796cba261af92157cea93f
github username <github email address>
2020-03-04 11:39:52 -0700

    getting user login and save to localStroage

--- app.js
+++ app.js
@@ -1,97 +1,138 @@
 console.log("biking")
 
 // users in localStorage
-const userURL = `localhost:3000/users`
-const loginURL = `localhost:3000/login`
+const userURL = `http://localhost:3000/users`
+const loginURL = `http://localhost:3000/login`
+const signupForm = document.getElementById('signup-form')
+const loginForm = document.getElementById('login-form')
+
+loginForm.addEventListener('submit', loginUser)
+signupForm.addEventListener('submit', signupUser)
+
+
+function signupUser() {
+  event.preventDefault()
+
+  const formData = new FormData(signupForm)
+  const username = formData.get('username')
+  const password = formData.get('password')
+
+
+}
+
+function loginUser() {
+
+  event.preventDefault()
+
+  const formData = new FormData(loginForm)
+  const username = formData.get('username')
+  const password = formData.get('password')
+
+  fetch(loginURL, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password
+      })
+    })
+    .then(response => response.json())
+    .then(result => {
+      localStorage.setItem('token', result.token)
+    })
+}
 
 
 // Weather 
-const weatherURL = `https://api.darksky.net/forecast/4bb777ada8dd0c5341cacfbcfb6a9225/37.8267,-105.7821`
-const cors = `https://cors-anywhere.herokuapp.com`
+// const weatherURL = `https://api.darksky.net/forecast/4bb777ada8dd0c5341cacfbcfb6a9225/37.8267,-105.7821`
+// const cors = `https://cors-anywhere.herokuapp.com`
 
-fetch(`${cors}/${weatherURL}`)
-    .then(response => response.json())
-    .then(result => {
-        currentWeather(result)
-        todaysWeather(result)
-    })
+// fetch(`${cors}/${weatherURL}`)
+//     .then(response => response.json())
+//     .then(result => {
+//         currentWeather(result)
+//         todaysWeather(result)
+//     })
 
-function currentWeather(result) {
-    const itsCurrently = document.getElementById('its-currently')
-    const current = document.createElement('h3')
+// function currentWeather(result) {
+//     const itsCurrently = document.getElementById('its-currently')
+//     const current = document.createElement('h3')
 
-    current.textContent = result.currently.summary
+//     current.textContent = result.currently.summary
 
-    itsCurrently.appendChild(current)
-}
+//     itsCurrently.appendChild(current)
+// }
 
-function todaysWeather(result) {
-    const today = document.getElementById('today')
-    const hourly = document.createElement('h2')
+// function todaysWeather(result) {
+//     const today = document.getElementById('today')
+//     const hourly = document.createElement('h2')
 
-    hourly.textContent = result.hourly.summary
+//     hourly.textContent = result.hourly.summary
 
-    today.appendChild(hourly)
-}
+//     today.appendChild(hourly)
+// }
 
 
 // Post
-const postURL = "http://localhost:3000/posts"
-const postContainer = document.getElementById('post-container')
-const postForm = document.getElementById('create-post')
-fetch(postURL)
-    .then(response => response.json())
-    .then(listPost)
+// const postURL = "http://localhost:3000/posts"
+// const postContainer = document.getElementById('post-container')
+// const postForm = document.getElementById('create-post')
+// fetch(postURL)
+//     .then(response => response.json())
+//     .then(listPost)
 
-function listPost(object) {
-    object.post.map(individualPost => {
-        const title = document.createElement('h2')
-        const description = document.createElement('li')
+// function listPost(object) {
+//     object.post.map(individualPost => {
+//         const title = document.createElement('h2')
+//         const description = document.createElement('li')
 
-        description.textContent = individualPost.description
-        title.textContent = individualPost.title
+//         description.textContent = individualPost.description
+//         title.textContent = individualPost.title
 
-        postContainer.append(title, description)
-    })
-}
+//         postContainer.append(title, description)
+//     })
+// }
 
-postForm.addEventListener('submit', userPost)
+// postForm.addEventListener('submit', userPost)
 
-function userPost() {
-    event.preventDefault()
+// function userPost() {
+//     event.preventDefault()
 
-    const formData = new FormData(postForm)
-    const title = formData.get('title')
-    const description = formData.get('description')
+//     const formData = new FormData(postForm)
+//     const title = formData.get('title')
+//     const description = formData.get('description')
 
-    fetch(postURL, {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          title: title,
-          description: description
-        })
+//     fetch(postURL, {
+//         method: 'POST',
+//         headers: {
+//           'Accept': 'application/json',
+//           'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({
+//           title: title,
+//           description: description
+//         })
 
-      })
-      .then(response => response.json())
-      .then(result => {
-          console.log(result)
-        appendNewPost(result)
-      })
-  }
+//       })
+//       .then(response => response.json())
+//       .then(result => {
+//           console.log(result)
+//         appendNewPost(result)
+//       })
+//   }
 
-  function appendNewPost(result) {
-    postForm.reset()
+//   function appendNewPost(result) {
+//     postForm.reset()
 
-    const newPostTitle = document.createElement('h2')
-    const newPosttDiscription = document.createElement('li')
+//     const newPostTitle = document.createElement('h2')
+//     const newPosttDiscription = document.createElement('li')
 
-    newPosttTitle.textContent = result.title
-    newPosttDiscription.textContent = result.title
+//     newPosttTitle.textContent = result.title
+//     newPosttDiscription.textContent = result.title
 
 
-    postContainer.append(newPostTitle, newPosttDiscription)
-  }
+//     postContainer.append(newPostTitle, newPosttDiscription)
+//   }
