5ef1ce99dd19b890c67e34e807ecb69cf386e06d
github username <github email address>
2020-03-04 16:17:13 -0700

    Getting google maps api to work

--- app.js
+++ app.js
@@ -62,7 +62,8 @@ function loginUser() {
     })
 }
 //Check local storage for user token
-if (!localStorage.token) {
+if (localStorage.token !== "undefined") {
+  mainPageLoad()
   // alert('Something went wrong please try again')
 } else {
   logoutButton()
@@ -82,8 +83,54 @@ function logoutUser() {
   localStorage.removeItem('token')
 }
 
+function mainPageLoad() {
+  const mainPage = document.getElementById('main-page')
+  console.log("main", mainPage)
+  // window.location.reload()
+  mainPage.style.display = 'block'
+}
+
+function initMap() {
+  var directionsService = new google.maps.DirectionsService();
+  var directionsRenderer = new google.maps.DirectionsRenderer();
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 7,
+    center: {
+      lat: 38.8267,
+      lng: -105.7821
+    }
+  });
+  directionsRenderer.setMap(map);
+
+  var onChangeHandler = function () {
+    calculateAndDisplayRoute(directionsService, directionsRenderer);
+  };
+  document.getElementById('start').addEventListener('change', onChangeHandler);
+  document.getElementById('end').addEventListener('change', onChangeHandler);
+}
+
+function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+  directionsService.route({
+      origin: {
+        query: document.getElementById('start').value
+      },
+      destination: {
+        query: document.getElementById('end').value
+      },
+      travelMode: 'BICYCLING'
+    },
+    function (response, status) {
+      if (status === 'OK') {
+        directionsRenderer.setDirections(response);
+      } else {
+        window.alert('Directions request failed due to ' + status);
+      }
+    });
+}
+
 
 // Weather 
+
 // const weatherURL = `https://api.darksky.net/forecast/4bb777ada8dd0c5341cacfbcfb6a9225/37.8267,-105.7821`
 // const cors = `https://cors-anywhere.herokuapp.com`
 
@@ -151,6 +198,7 @@ function logoutUser() {
 //         body: JSON.stringify({
 //           title: title,
 //           description: description
+//            user_id: 
 //         })
 
 //       })
